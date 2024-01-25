4073cf6904621029355de87817890cda2db74265
Verda Muso <gearmobile@gmail.com>
2024-01-25 21:23:56 +0300

    add main logic

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, WritableSignal, inject } from '@angular/core';
+
+import { TodosService } from '@services/todos.service';
+
+import { ITodo } from '../../shared/types';
 
 @Component({
   selector: 'td-main',
@@ -8,4 +12,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {}
+export class MainComponent {
+  private readonly _todosService: TodosService = inject(TodosService);
+
+  public todos: WritableSignal<ITodo[]> = this._todosService.todos;
+}
